88760cebaf16c842f56816abd665eeab13a2bb0b
Olga Ivanycheva <oly3998686@yandex.ru>
2023-06-27 19:06:56 +0300

    second v

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,24 @@
+const { configError } = require('../utils/configError');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((user) => res.send({ data: user }))
-    .catch((err) => Error(res, err));
+    .catch((err) => configError(res, err));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
     .orFail(new Error('notValidId'))
     .then((user) => res.send({ data: user }))
-    .catch((err) => Error(res, err));
+    .catch((err) => configError(res, err));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => Error(res, err));
+    .catch((err) => configError(res, err));
 };
 
 module.exports.updateUser = (req, res) => {
@@ -33,7 +34,7 @@ module.exports.updateUser = (req, res) => {
   )
     .orFail(new Error('notValidId'))
     .then((user) => res.send({ data: user }))
-    .catch((err) => Error(res, err));
+    .catch((err) => configError(res, err));
 };
 
 module.exports.updateUserAvatar = (req, res) => {
@@ -49,5 +50,5 @@ module.exports.updateUserAvatar = (req, res) => {
   )
     .orFail(new Error('notValidId'))
     .then((user) => res.send({ data: user }))
-    .catch((err) => Error(res, err));
+    .catch((err) => configError(res, err));
 };
