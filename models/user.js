99e279a6818de6922646471b194175b2b53d2e77
Olga Ivanycheva <oly3998686@yandex.ru>
2023-07-03 16:30:33 +0300

    14 sprint fourth v

--- models/user.js
+++ models/user.js
@@ -1,43 +1,46 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
+const isUrl = require('validator/lib/isURL');
 
 const validate = /^(https|http)?:\/\/(www.)?[^-_.\s](\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})?(:\d+)?(.+[#a-zA-Z/:0-9]{1,})?\.(.+[#a-zA-Z/:0-9]{1,})?$/i;
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: false,
     default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: false,
-    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь океана',
   },
   avatar: {
     type: String,
-    required: false,
     match: validate,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (url) => isUrl(url),
+      message: 'Некорректный адрес URL',
+    },
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
     validate: {
       validator: (valid) => isEmail(valid),
       message: 'Некорректный формат e-mail',
     },
-    required: true,
-    unique: true,
   },
   password: {
     type: String,
     select: false,
     required: true,
+    minlength: 8,
   },
 });
 
